4d835a288dd72d0589d04cab45af678c401ad1d1
潘少鹏 <v-panshaopeng@xiaomi.com>
2025-10-19 15:08:44 +0800

    add func

--- usermanager/auth.go
+++ usermanager/auth.go
@@ -1,10 +1,17 @@
 // 验证用户名和密码的代码
 package usermanager
 
-import "golang.org/x/crypto/bcrypt"
+import (
+	"fleetpilot/common/logger"
+
+	"golang.org/x/crypto/bcrypt"
+)
 
 // 比对验证密码
 func ComparePass(sourcepass, verifypass string) bool {
 	err := bcrypt.CompareHashAndPassword([]byte(sourcepass), []byte(verifypass))
+	if err != nil {
+		logger.Error("compare password error:%v", err)
+	}
 	return err == nil
 }
