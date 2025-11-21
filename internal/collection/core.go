d23371330fd9697947fd4801bcbabb514cbecc38
潘少鹏 <v-panshaopeng@xiaomi.com>
2025-11-21 16:37:38 +0800

    test push

--- internal/collection/core.go
+++ internal/collection/core.go
@@ -71,7 +71,7 @@ type UrlInfo struct {
 	InjectionType string      `json:"injectionType"  form:"injectionType" gorm:"column:tag"`
 	InjectionPath StringSlice `json:"injectionPath"  form:"injectionPath" gorm:"column:directories"`
 	Domains       StringSlice `json:"domains" form:"domains" gorm:"column:domains"`
-	Ports         IntSlice    `json:"ports" gorm:"column:ports;type:JSON"`
+	Ports         IntSlice    `json:"ports" form:"ports" gorm:"column:ports;serializer:json"`
 }
 
 func (c *UrlInfo) InsertData(id int64, date JSONTime, url, injectionType string, injectionPath, domains StringSlice, ports IntSlice) error {
